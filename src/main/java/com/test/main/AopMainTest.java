59ef9596993a949436ac174ade0d763701ac2bf2
songchongyang.scy <songchongyang.scy@alibaba-inc.com>
2023-08-26 17:11:09 +0800

    test: 支持 FactoryBean，实现非侵入式的动态代理

--- src/main/java/com/test/main/AopMainTest.java
+++ src/main/java/com/test/main/AopMainTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.minis.beans.factory.annotation.Autowired;
 import com.minis.web.bind.annotation.RequestMapping;
 import com.minis.web.bind.annotation.ResponseBody;
 import com.test.aop.service.ActionOne;
@@ -15,8 +16,12 @@ import com.test.aop.dynamicProxy.DynamicProxyHelper;
 
 public class AopMainTest {
 
+    @Autowired
+    private ActionOne actionOne;
+
+
     /**
-     * 访问：http://localhost:8080/scy_mini_spring_war_exploded//aop/dynamicProxy?msg=scy
+     * 访问：http://localhost:8080/scy_mini_spring_war_exploded/aop/dynamicProxy?msg=scy
      */
     @RequestMapping("/aop/dynamicProxy")
     @ResponseBody
@@ -35,6 +40,21 @@ public class AopMainTest {
 
         try {
             String result = "one: " + resultOne + "\n" + "two: " + resultTwo;
+            response.getWriter().write(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 访问：http://localhost:8080/scy_mini_spring_war_exploded/aop/factoryBean?msg=scy
+     */
+    @RequestMapping("/aop/factoryBean")
+    @ResponseBody
+    public void doTest2(HttpServletRequest request, HttpServletResponse response) {
+        String msg = request.getParameter("msg");
+        String result = actionOne.doActionOne(msg);
+        try {
             response.getWriter().write(result);
         } catch (IOException e) {
             e.printStackTrace();
