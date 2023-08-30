bd9d9dd493c03c8133819dc343f0b4883feea444
songchongyang.scy <songchongyang.scy@alibaba-inc.com>
2023-08-30 21:29:31 +0800

    test: 结构化 AOP 的增强操作

---
+++ src/main/java/com/test/aop/LoggingInterceptor.java
@@ -0,0 +1,23 @@
+package com.test.aop;
+
+import com.minis.aop.MethodInterceptor;
+import com.minis.aop.MethodInvocation;
+
+/**
+ * 日志增强：在方法调用前后都打印日志
+ */
+public class LoggingInterceptor implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        System.out.println("LoggingInterceptor. method " + invocation.getMethod() + " is called on " +
+            invocation.getThis() + " with args " + invocation.getArguments());
+
+        Object result = invocation.proceed();
+
+        System.out.println("LoggingInterceptor. method " + invocation.getMethod() + " returns " + result);
+
+        return result;
+    }
+
+}
